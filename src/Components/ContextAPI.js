a8231f4e13eaa32d60f57416b5371d5fd86128ee
Eashan Banerjee <55328960+Ph1Eashan@users.noreply.github.com>
2022-01-09 06:25:06 +0530

    Update ContextAPI.js

--- src/Components/ContextAPI.js
+++ src/Components/ContextAPI.js
@@ -3,10 +3,10 @@ export const ContextAPI = createContext();
 
 export const Studentdata = (props) => {
   const [rows, setRows] = useState([
-    { id: "1", name: "Alpha", age: 26, course: "MERN", batch: "August"},
-    { id: "2", name: "Charlie", age: 23, course: "MERN", batch: "September"},
-    { id: "3", name: "Bravo", age: 21, course: "MERN", batch: "October"},
-    { id: "4", name: "Tango", age: 25, course: "MERN", batch: "November"},
+    { id: "1", name: "Jack", age: 25, course: "MERN", batch: "January"},
+    { id: "2", name: "Sparrow", age: 26, course: "MERN", batch: "March"},
+    { id: "3", name: "James", age: 21, course: "MERN", batch: "May"},
+    { id: "4", name: "Bond", age: 22, course: "MERN", batch: "June"},
   ])
   return (
     <ContextAPI.Provider value={[rows, setRows]}>
